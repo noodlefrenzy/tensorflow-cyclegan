8ae24dc082a3c5c00ddeb789f5918b1c35522ca6
Michael Lanzetta <milanz@microsoft.com>
2017-05-02 21:32:15 -0700

    Remove ImageCache until I can figure out how to add it back properly.

--- cyclegan.py
+++ cyclegan.py
@@ -107,32 +107,6 @@ class Images():
         image.set_shape([self.image_size, self.image_size, 3])
         return image
 
-
-class ImageCache:
-    def __init__(self, cache_size=30):
-        self.cache_size = cache_size
-        self.images = []
-
-    def fetch(self, image):
-        if self.cache_size == 0:
-            return image
-
-        p = random.random()
-        if p > 0.5 and len(self.images) > 0:
-            # use and replace old image.
-            random_id = random.randrange(len(self.images))
-            retval = self.images[random_id].copy()
-            if len(self.images) < self.cache_size:
-                self.images.append(image.copy())
-            else:
-                self.images[random_id] = image.copy()
-            return retval
-        else:
-            if len(self.images) < self.cache_size:
-                self.images.append(image.copy())
-            return image
-
-
 # DEFINE OUR LOAD DATA OPERATIONS
 # -------------------------------------------------------
 # DEFINE OUR SAMPLING FUNCTIONS
